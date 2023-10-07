5abdb829aee666ffe79f7db2a7ad6b2a48044b75
HELLOHIDI <fbgmlwo123@naver.com>
2023-10-07 14:11:45 +0900

    Initial Commit

---
+++ DoSopt_RyuHeeJae_pratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoSopt_RyuHeeJae_pratice
+//
+//  Created by 류희재 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
